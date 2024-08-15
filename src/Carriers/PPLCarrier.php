dbfa29c16486570c9f40c8b4496800924c2cab66
Dmytro Verdi <dmytro.verdi@martinus.sk>
2024-08-15 17:09:49 +0200

    add tracking url

--- src/Carriers/PPLCarrier.php
+++ src/Carriers/PPLCarrier.php
@@ -9,8 +9,15 @@ class PPLCarrier extends Carrier
 {
     public const NAME = 'PPL';
 
-    public const REGEX = '/^[0-9]$/i';
+    public const REGEX = '/^([0-9]+)$/i';
 
     protected $endPointClass = PPLEndpoint::class;
+
+    public function getTrackingUrl(string $number): string
+    {
+        $trackingUrl = 'https://www.ppl.cz/vyhledat-zasilku?shipmentId=$1';
+
+        return preg_replace(self::REGEX, $trackingUrl, $number);
+    }
 
 }
